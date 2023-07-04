d645ce65041a3c3e9b115843b757f7edb014cc46
MarcosCampodonico <marcoscampodonico4550@outlook.com>
2023-07-04 15:33:06 -0300

    lista de angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,37 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'interpolacion';
+  numero = 30;
+  hayerror=true;
+  miFontSyze='50px';
+  status = 1;
+  data:Array<any>;
+
+  constructor(){
+    this.data = [
+      {nombre: 'marcos', apellido:'campodonico', edad:23},
+      {nombre: 'luis', apellido:'campodonico', edad:56},
+      {nombre: 'johanna', apellido:'campodonico', edad:20},
+      {nombre: 'jesus', apellido:'flores', edad:27}
+    ]
+  }
+
+
+  // constructor() {
+  //   console.log('constructor');
+  //   setInterval(() => {
+  //     this.numero++
+  //   },1000)
+  // }
+  // onClick(event: MouseEvent): void{
+  //   console.log(event);
+  // }
+  // onInputChange(event: Event): void{
+  //   console.log(event);
+  // }
+
+
 }
+
+  
+
